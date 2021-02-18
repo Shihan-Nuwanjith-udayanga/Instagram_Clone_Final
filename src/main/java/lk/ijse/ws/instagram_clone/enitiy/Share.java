e03d9e7cc544dc1a5156fcb7ea24a2cd551de636
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2021-02-18 22:31:25 +0530

    Create Share entity

---
+++ src/main/java/lk/ijse/ws/instagram_clone/enitiy/Share.java
@@ -0,0 +1,18 @@
+package lk.ijse.ws.instagram_clone.enitiy;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+public class Share {
+
+    @Id
+    private Long id;
+    @ManyToOne
+    private InstaUser sharedUser;
+    @ManyToOne
+    private Post post;
+    private Date time;
+}
