b24180272be2a93d3ebb5eef44c09bdf34a8b316
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2021-02-18 22:28:39 +0530

    Create Comment entity

---
+++ src/main/java/lk/ijse/ws/instagram_clone/enitiy/Comment.java
@@ -0,0 +1,19 @@
+package lk.ijse.ws.instagram_clone.enitiy;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+public class Comment {
+
+    @Id
+    private Long id;
+    @ManyToOne
+    private InstaUser commenter;
+    @ManyToOne
+    private Post post;
+    private String comment;
+    private Date time;
+}
