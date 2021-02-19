4fcd8dfdda254fdd28dd1ac18a021e88773dc052
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2021-02-19 20:17:19 +0530

    Create AwsCognitoJwtAuthFilter

---
+++ src/main/java/lk/ijse/ws/instagram_clone/jwt/AwsCognitoJwtAuthFilter.java
@@ -0,0 +1,13 @@
+package lk.ijse.ws.instagram_clone.jwt;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class AwsCognitoJwtAuthFilter extends GenericFilter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+
+}
